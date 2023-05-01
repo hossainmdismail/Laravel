76ce0dae7ad13d1781fe7393cf3acf7d155c25bc
Ismail <hossainmdismail9010@gmail.com>
2023-05-02 04:04:01 +0600

    Vite Solutions

---
+++ UI Kit Bootstrap/bootstrapkit.php
@@ -0,0 +1,23 @@
+<?php
+
+//Install laravel ui
+composer require laravel/ui:^1.0 --dev
+
+// Generate basic scaffolding...
+//    php artisan ui bootstrap
+//    php artisan ui vue
+//    php artisan ui react
+
+// Generate login / registration scaffolding...
+php artisan ui bootstrap --auth
+//    php artisan ui vue --auth
+//    php artisan ui react --auth
+
+//building vite solution
+npm install -save-dev vite laravel-vite-plugin
+
+
+//setUp
+npm run build
+
+ ?>
