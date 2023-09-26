7ab36f5c2e4800c819d25523b21b81751b99debf
esmail <120841700+hossainmdismail@users.noreply.github.com>
2023-09-26 11:55:49 -0700

    Document

--- Helper/F_uploadPhoto.php
+++ Helper/F_uploadPhoto.php
@@ -1,6 +1,18 @@
+// Create a folder on your app/helper/
+// Go to composer.json and updae autoload
+
+// "files": [
+// "app/helper/photo.php"
+// ],
+
+// Reset your server again
+
 <?php
+
 use Intervention\Image\ImageManagerStatic as Image;
-class Photo{
+
+class Photo
+{
     //Return Value
     public static $name;
     function __construct()
@@ -8,30 +20,25 @@ class Photo{
         echo ' hi';
     }
     //Image Upload Methods
-    public static function upload($file,$path,$prefix,$size=[]){
+    public static function upload($file, $path, $prefix, $size = [])
+    {
         if (file_exists(public_path($path))) {
             try {
                 $extention = $file->getClientOriginalExtension();
-                $name = $prefix.rand(1,2000).rand(1,500).'-'.date('dmy').'.'.$extention; //Name generator
-                if(count($size) != 2){ //Check size exists
-                    Image::make($file)->save(public_path($path.'/'.$name));
-                }else{
-                    Image::make($file)->resize($size[0],$size[1])->save(public_path($path.'/'.$name));
+                $name = $prefix . rand(1, 2000) . rand(1, 500) . '-' . date('dmy') . '.' . $extention; //Name generator
+                if (count($size) != 2) { //Check size exists
+                    Image::make($file)->save(public_path($path . '/' . $name));
+                } else {
+                    Image::make($file)->resize($size[0], $size[1])->save(public_path($path . '/' . $name));
                 }
-                self::$name = $name;// get Last upload Name
+                self::$name = $name; // get Last upload Name
                 return true;
-                }
-            catch (\Throwable $th) {
+            } catch (\Throwable $th) {
                 return false;
-                }
-        }else {
-          return false;
+            }
+        } else {
+            return false;
         }
     }
-
 }
 ?>
-
-
-
-
