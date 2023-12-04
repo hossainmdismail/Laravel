fdf274a7f0fb5bc306bba4f821a833e076675dee
esmail <120841700+hossainmdismail@users.noreply.github.com>
2023-12-04 13:12:12 +0600

    Passport error

--- API/Validation/post_validations.php
+++ API/Validation/post_validations.php
@@ -13,41 +13,41 @@ class UserController extends Controller
     public function store(Request $request)
     {
         // Request Validation
-       $validator = Validator::make($request->all(), [
-        'name'      => ['required'],
-        'email'     => ['required','unique:users,email'],
-        'password'  => ['required'],
-       ]);
-       if ($validator->fails()) {
-       // Request Error Pass
-        return response()->json($validator->messages(), 400);
-       }else {
-        // Insert Data    
-        $data = [
-            'name'      => $request->name,
-            'email'     => $request->email,
-            'password'  => bcrypt($request->password),
-        ];
-        
-        DB::beginTransaction(); //Notify that data going to be insert 
+        $validator = Validator::make($request->all(), [
+            'name'      => ['required'],
+            'email'     => ['required', 'unique:users,email'],
+            'password'  => ['required'],
+        ]);
+        if ($validator->fails()) {
+            // Request Error Pass
+            return response()->json($validator->messages(), 400);
+        } else {
+            // Insert Data    
+            $data = [
+                'name'      => $request->name,
+                'email'     => $request->email,
+                'password'  => bcrypt($request->password),
+            ];
 
-        try {
-            $user =  User::create($data);
-            DB::commit(); //Insert Data
-        } catch (\Exception $th) {
-            DB::rollBack(); //If any error found it will remove bulk data 
-            $user = null; //To verify and respons succsess data
+            DB::beginTransaction(); //Notify that data going to be insert 
+
+            try {
+                $user =  User::create($data);
+                DB::commit(); //Insert Data
+            } catch (\Exception $th) {
+                DB::rollBack(); //If any error found it will remove bulk data 
+                $user = null; //To verify and respons succsess data
+            }
+            if ($user != null) {
+                return response()->json([
+                    'message' => 'User registerd successfully',
+                ], 200);
+            } else {
+                return response()->json([
+                    'message' => 'Internal server error',
+                ], 500);
+            }
         }
-        if ($user != null) {
-            return response()->json([
-                'message' => 'User registerd successfully',
-            ],200);
-        }else {
-            return response()->json([
-                'message' => 'Internal server error',
-            ],500);
-        }
-       }
     }
 }
 
---
+++ API/passport/installation.php
@@ -0,0 +1,21 @@
+<!-- How to install passport in laravel 10 -->
+
+
+<!-- ==================
+
+#1 
+I had to manually add the package to the require section of my composer.json file to use the latest version (11) of Passport:
+
+#2
+composer update
+
+#3
+php artisan migrate
+
+#4
+php artisan passport:install
+
+important thing is 1,2 for install passport in laravel 10 ,
+after the installation process of 1,2 you can follow laravel documentation as well
+
+==============>
